f80cd211a754d511b12e14dae035f35f38fd621d
nfm25 <49427856+nfm25@users.noreply.github.com>
2019-04-12 00:49:37 +0900

    에셋 추가 및 게임 화면 구성

---
+++ Unity 2D Rhythm Game/Assets/Scripts/BackgroundBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundBehavior : MonoBehaviour
+{
+    public GameObject gameBackground;
+    private SpriteRenderer gameBackgroundSpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameBackgroundSpriteRenderer = gameBackground.GetComponent<SpriteRenderer>();
+        StartCoroutine(FadeOut(gameBackgroundSpriteRenderer, 0.005f));
+    }
+
+   IEnumerator FadeOut(SpriteRenderer spriteRenderer, float amount)
+    {
+        Color color = spriteRenderer.color;
+        while(color.a > 0.0f) // 0.0f == 완전투명, 따라서 완전투명이 아닌 동안은..
+        {
+            color.a -= amount;
+            spriteRenderer.color = color;
+            yield return new WaitForSeconds(amount);
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
